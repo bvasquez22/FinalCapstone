02e182790542e1a251415e2617e32fc2bc709822
Brycen Vasquez <brycenvasquez22@gmail.com>
2023-12-16 12:01:21 -0700

    Small bug fixes and added basic home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Home.css';
+
+const Home = () => {
+    return (
+        <body className='home_page'>
+            <div className="intro_text">
+                <p>Welcome to <span className='fab_name'>FaB Manager</span> — your ultimate companion for mastering
+                your Flesh and Blood card collection. 
+                </p>
+            </div>
+        </body>
+    )
+};
+
+export default Home
