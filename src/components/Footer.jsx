b081eb6f311c797f84e900bc39323a9ca3b561f0
Brycen Vasquez <brycenvasquez22@gmail.com>
2023-12-18 19:59:28 -0700

    Footer added and styled

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span className="fab_name">FaB Manager</span> is in no way affiliated
+        with <a href="https://legendstory.com/">Legend Story Studios</a>®.{" "}
+        <a href="https://fabtcg.com/">Flesh and Blood</a>™ is a registered
+        trademark of Legend Story Studios. Flesh and Blood™ and all associated
+        images are property of Legend Story Studios. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
