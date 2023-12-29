66fde0458ad77b30b9880692e1756a649af9529b
Brycen Vasquez <brycenvasquez22@gmail.com>
2023-12-28 18:13:09 -0800

    Dropdown menu added - debugging

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+import SetDropdown from "./SetDropdown";
 
 import './Header.css';
 
 const Header = () => {
+    const [isDropdownVisible, setDropdownVisible] = useState(false);
+    const [DropdownValue, setDropdownValue] = useState('');
+
+    const handleMouseEnter = (event) => {
+        setDropdownVisible(true);
+        setDropdownValue(event.target.value)
+    };
+
+    const handleMouseLeave = () => {
+        setDropdownVisible(false);
+        setDropdownValue('')
+    };
+
     return (
         <header>
             <nav className="fab_nav">
@@ -12,8 +28,14 @@ const Header = () => {
                         <h1>FaB Manager</h1>
                     </Link>
                 </div>
-                <Link to='/sets' className="nav_link">
+                <Link
+                    to={`/${DropdownValue}`}
+                    className="nav_link"
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
                     <h4>Sets</h4>
+                    {isDropdownVisible && <SetDropdown />}
                 </Link>
                 <Link to='my-collection' className="nav_link">
                 {/* <Link to={`/my-collection/${user.id}`}> */}
---
+++ src/components/SetDropdown.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './SetDropdown.css'
+
+const SetDropdown = () => {
+  return (
+    <div className="set_dropdown">
+      <ul>
+        <li>Welcome to Rathe</li>
+        <li>Arcane Rising</li>
+        <li>Crucible of War</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SetDropdown;
